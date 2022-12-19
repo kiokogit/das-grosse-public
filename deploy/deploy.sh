6af559d02a6320471844c4f839f390b2c9b4141d
kiokogit <kiokovincent12@gmail.com>
2022-12-19 10:01:06 +0300

    deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,8 +4,8 @@ DEPLOY_SERVER=$DEPLOY_SERVER
 SERVER_FOLDER="html-folder-in-server"
 
 # Building React output for the sake of the projectb
-yarn install
-yarn run build
+npm install
+npm run build
 
 echo "Deploying to ${DEPLOY_SERVER}"
 scp -r build/ root@${DEPLOY_SERVER}:/var/www/html/${SERVER_FOLDER}/
